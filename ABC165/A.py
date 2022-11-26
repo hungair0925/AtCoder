4e9192e4bccb54b31a1198b4866f365c4bec34a2
hungair0925 <hungair0925@gmail.com>
2022-11-26 16:50:54 +0900

    ABC165_A

---
+++ ABC165/A.py
@@ -0,0 +1,16 @@
+k = int(input())
+a, b = map(int, input().split())
+index = 1
+
+while True:  
+    killo_meter = k * index
+    
+    if killo_meter > b:
+        print("NG")
+        break
+
+    if killo_meter >= a and killo_meter <= b:
+        print("OK")
+        break
+    
+    index += 1
