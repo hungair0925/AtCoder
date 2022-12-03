97e3654e5d6670ac247a79b1309ba59599e1679f
hungair0925 <hungair0925@gmail.com>
2022-12-03 23:51:19 +0900

    ABC280_D_TLE

--- ABC280/D.py
+++ ABC280/D.py
@@ -1,11 +1,38 @@
-# Not AC
+# TLE
 
 k = int(input())
 
-factorial_amount = 1
 
-for i in range(1, k + 1):
-    factorial_amount *= i
-    if factorial_amount % k == 0:
-        print(i)
-        break
+def is_prime(n):
+    if n <= 1:
+        return False
+    
+    for i in range(2, n):
+        if n % i == 0: 
+            return False
+    return True
+
+def list_primes(n):
+    primes = []
+    for i in range(2, n):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+if is_prime(k):
+    print(k)
+else:
+    primes = list_primes(k)
+    divide_primses = []
+
+    index = 0
+    while k not in primes:
+        if k % primes[index] > 0:
+            index += 1
+            continue
+        k = k // primes[index]
+        divide_primses.append(primes[index])
+    divide_primses.append(k)
+    print(divide_primses[-1])
+        
+
