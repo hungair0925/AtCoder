4285717a68c4371d229a5421fed6fe629fecbf18
hungair0925 <hungair0925@gmail.com>
2022-12-05 00:04:14 +0900

    modify snippet

--- snippet.py
+++ snippet.py
@@ -1,2 +1,11 @@
+def input_int():
+    return int(input())
+
+def input_str():
+    return input()
+
 def multi_input_int():
     return map(int, input().split())
+
+def multi_input_str():
+    return input().split()
